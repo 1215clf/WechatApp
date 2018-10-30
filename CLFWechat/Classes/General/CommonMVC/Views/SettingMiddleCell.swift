eef6815c5eb07fbfae25e84b205cf74f831457a1
1215clf <1210680838@qq.com>
2018-10-30 19:29:16 +0800

    SettingMiddleCell

--- CLFWechat/Classes/General/CommonMVC/Views/SettingMiddleCell.swift
+++ CLFWechat/Classes/General/CommonMVC/Views/SettingMiddleCell.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 class SettingMiddleCell: SettingBaseCell {
+    override var model: SettingCellModel?{
+        didSet{
+            self.setModel()
+        }
+        
+    }
     
+    lazy var titleLabel:UILabel = {
+       let titleL = UILabel()
+        return titleL
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        titleLabel.snp.makeConstraints { (make) in
+            make.left.top.right.bottom.equalTo(self)
+        }
+        
+        self.addSubview(titleLabel)
+        
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("nit?(coder:) has not been implemented")
+    }
+}
+extension SettingMiddleCell{
+    fileprivate func setModel(){
+        titleLabel.text = model?.title
+    }
 }
